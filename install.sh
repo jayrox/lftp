23d8700f3227cd5d3ecbcf08d5be4283626fbdf2
Scottst <scott@stevensononthe.net>
2015-10-18 11:31:01 -0400

    Update install.sh
    Re-structured the flow to use a control script for the variables.  That way the main FTP script can be user modified and it wont get overwritten when the docker is modified.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # https://github.com/gfjardim/
 #
 # Created Oct 4, 2015
-# Modified Oct 5, 2015
+# Modified Oct 18, 2015
 #
 #########################################
 ##        ENVIRONMENTAL CONFIG         ##
@@ -38,7 +38,7 @@ apt-get install -qy -f lftp \
 cat <<'EOT' > /etc/my_init.d/config.sh
 #!/bin/bash
 
-# Find FTP_PORT, FTP_USER, FTP_PASSWORD, FTP_HOST, FTP_REMOTE_DIR in the ftpsync script and replace it with the environment variable
+# Find FTP_PORT, FTP_USER, FTP_PASSWORD, FTP_HOST, FTP_REMOTE_DIR in the control script and replace it with the environment variable
 
 # If the port environment variable is not set assume port 990
 if [[ -z $FTP_PORT ]]; then
@@ -50,103 +50,133 @@ if [[ -z $FTP_REMOTE_DIR ]]; then
   FTP_REMOTE_DIR="/."
 fi
 
-# Replace variables in the script
-sed -i -e "s#FTP_PORT#${FTP_PORT}#" /opt/syncftp.sh
-sed -i -e "s#FTP_USER#${FTP_USER}#" /opt/syncftp.sh
-sed -i -e "s#FTP_PASSWORD#${FTP_PASSWORD}#" /opt/syncftp.sh
-sed -i -e "s#FTP_HOST#${FTP_HOST}#" /opt/syncftp.sh
-sed -i -e "s#FTP_REMOTE_DIR#${FTP_REMOTE_DIR}#" /opt/syncftp.sh
+# Replace variables in the control script
+sed -i -e "s#FTP_PORT#${FTP_PORT}#" /etc/lftp/syncControl.sh
+sed -i -e "s#FTP_USER#${FTP_USER}#" /etc/lftp/syncControl.sh
+sed -i -e "s#FTP_PASSWORD#${FTP_PASSWORD}#" /etc/lftp/syncControl.sh
+sed -i -e "s#FTP_HOST#${FTP_HOST}#" /etc/lftp/syncControl.sh
+sed -i -e "s#FTP_REMOTE_DIR#${FTP_REMOTE_DIR}#" /etc/lftp/syncControl.sh
 
 # If no cron time is specified use daily at 1AM
 if [[ -z $FTP_CRON_JOB ]]; then
   $FTP_CRON_JOB="0 1 * * *"
 fi
 
-# Copy the bash script to the unraid mounted folder (Is this needed?)
-cp /opt/syncftp.sh /etc/lftp/syncftp.sh
+# Copy the ftp script to the unraid mounted folder
+# Check if the FTP script file exists in the mounted directory.  Only copy it if it does not exist.
+if [! -f "/mnt/lftp/syncftp.sh" ]
+then
+  cp /opt/syncftp.sh /mnt/lftp/syncftp.sh
+fi
 
-# Make sure the script is executable
-chmod +x /etc/lftp/syncftp.sh
+# Make sure the scripts are executable
+chmod +x /mnt/lftp/syncftp.sh
+chmod +x /etc/lftp/syncControl.sh
 
 # Add a cron to run the script
-FTP_CRON_JOB+=" /etc/lftp/syncftp.sh >> /etc/lftp/syncftp.log 2>&1"
+FTP_CRON_JOB+=" /etc/lftp/syncControl.sh >> /mnt/lftp/syncftp.log 2>&1"
 crontab -l | { cat; echo "$FTP_CRON_JOB"; } | crontab -
 EOT
 
+if [ ! -d "/etc/lftp" ]; then
+  mkdir -p /etc/lftp
+fi
 
-mkdir -p /etc/lftp
-
-# Sync FTP Script
-cat <<'EOT' > /opt/syncftp.sh
+# Create Sync Control Script
+cat <<'EOT' > /etc/lftp/syncControl.sh
 #!/bin/bash
-# This script will enter the FTP, mirror the completed directory to the locally mounted directory (unraid server mnt/cache/downloads share by default)
-# After downloading from the FTP it will remove the FTP files
-# It will then extract and RAR files that are CONTAINED in the root folder but not a subfolder of the folder. ie. it will not extract SUBS
-# It will then delete the RAR files
-# This script is designed to work with Scene Release file structures
-
 login=FTP_USER
 pass=FTP_PASSWORD
 host=FTP_HOST
 port=FTP_PORT
 remote_dir=FTP_REMOTE_DIR
-local_dir="/mnt/downloads"
-
-#lftp connection information.  May need to be modified for specific connection requirements
-lftp << EOF
-  set net:timeout 30
-  set net:max-retries 10
-  set net:reconnect-interval-base 5
-  set net:reconnect-interval-multiplier 5
-  set ftp:ssl-auth TLS
-  set ftp:ssl-force true
-  set ftp:ssl-protect-list yes
-  set ftp:ssl-protect-data yes
-  set ssl:verify-certificate off
-  set mirror:use-pget-n 5
-  open -p $port -u $login,$pass $host
-  cd "$remote_dir"
-  find . | grep [[:alnum:]] | sed -e 's~.~rmdir" "-f" "\".~' -e 's~$~\"~' | tac > /tmp/delete
-  mirror -c -P2 -vvv --no-empty-dirs --Remove-source-files $remote_dir $local_dir
-  source /tmp/delete
-  quit 0
-EOF
-
-  #start working in the mounted directory
-  cd $local_dir
-  #look for which folders exist.
-  for folder in */
-    #only try the following if there is a directory
-    do if [ -d "$folder" ]
-    then
-        #Enter each folder
-        cd "$folder"
-        #date
-                #Look for the filename ending in .rar
-                for filename in *.rar
-                        do echo "extracting $filename"
-                        #first do will extract the files from the rar and then delete the .rar file
-                        find . ! -name . -prune -type d -o -name "*.rar" -print -exec unrar e {} -y -o- \; -exec rm {} \;
-
-                        #other options to find to extract from
-                        #find . -name "*part01.rar" -exec unrar e {} -y -o- \;
-                        #find . -name "*part001.rar" -exec unrar e {} -y -o- \;
-                        #find . -name "*.r00" -exec unrar e {} -y -o- \;
-
-                        #this do will then delete all the *.r01, *.r02 etc.
-                        find . ! -name . -prune -type d -o -name "*.r[0-9]*[0-9]" -print -exec rm {} \;
-                        #I also dont use the SFV files so lets delete them also
-                        find . ! -name . -prune -type d -o -name "*.sfv" -print -exec rm {} \;
-                        #find . -name "*.r*" -exec rm {} \;
-                done
-        #go back up a directory
-         cd "..";
-        chmod -R 777 "$folder"
-    fi
-    done
-  exit 0
-fi
+  
+source /mnt/lftp/syncftp.sh
 EOT
+
+
+# Check if the FTP script file exists in the mounted directory.  Only create if it does not exist.
+if [! -f "/mnt/lftp/syncftp.sh" ]
+then
+  # Create Sync FTP Script
+  cat <<'EOT' > /opt/syncControl.sh
+  #!/bin/bash
+  #
+  # Variables are stored in /etc/lftp/syncControl.sh
+  # They are written from the docker into this file.
+  # The variables are: 
+  # login=FTP_USER
+  # pass=FTP_PASSWORD
+  # host=FTP_HOST
+  # port=FTP_PORT
+  # remote_dir=FTP_REMOTE_DIR
+  #
+  # This script is called from /etc/lftp/syncControl.sh
+  #
+  # This script will enter the FTP, mirror the completed directory to the locally mounted directory (unraid server mnt/cache/downloads share by default)
+  # After downloading from the FTP it will remove the FTP files
+  # It will then extract and RAR files that are CONTAINED in the root folder but not a subfolder of the folder. ie. it will not extract SUBS
+  # It will then delete the RAR files
+  # This script is designed to work with Scene Release file structures
+  
+  local_dir="/mnt/downloads"
+  
+  #lftp connection information.  May need to be modified for specific connection requirements
+  lftp << EOF
+    set net:timeout 30
+    set net:max-retries 10
+    set net:reconnect-interval-base 5
+    set net:reconnect-interval-multiplier 5
+    set ftp:ssl-auth TLS
+    set ftp:ssl-force true
+    set ftp:ssl-protect-list yes
+    set ftp:ssl-protect-data yes
+    set ssl:verify-certificate off
+    set mirror:use-pget-n 5
+    open -p $port -u $login,$pass $host
+    cd "$remote_dir"
+    find . | grep [[:alnum:]] | sed -e 's~.~rmdir" "-f" "\".~' -e 's~$~\"~' | tac > /tmp/delete
+    mirror -c -P2 -vvv --no-empty-dirs --Remove-source-files $remote_dir $local_dir
+    source /tmp/delete
+    quit 0
+  EOF
+  
+    #start working in the mounted directory
+    cd $local_dir
+    #look for which folders exist.
+    for folder in */
+      #only try the following if there is a directory
+      do if [ -d "$folder" ]
+      then
+          #Enter each folder
+          cd "$folder"
+          #date
+                  #Look for the filename ending in .rar
+                  for filename in *.rar
+                          do echo "extracting $filename"
+                          #first do will extract the files from the rar and then delete the .rar file
+                          find . ! -name . -prune -type d -o -name "*.rar" -print -exec unrar e {} -y -o- \; -exec rm {} \;
+  
+                          #other options to find to extract from (not used now)
+                          #find . -name "*part01.rar" -exec unrar e {} -y -o- \;
+                          #find . -name "*part001.rar" -exec unrar e {} -y -o- \;
+                          #find . -name "*.r00" -exec unrar e {} -y -o- \;
+  
+                          #this do will then delete all the *.r01, *.r02 etc.
+                          find . ! -name . -prune -type d -o -name "*.r[0-9]*[0-9]" -print -exec rm {} \;
+                          #I also dont use the SFV files so lets delete them also
+                          find . ! -name . -prune -type d -o -name "*.sfv" -print -exec rm {} \;
+                          #find . -name "*.r*" -exec rm {} \;
+                  done
+          #go back up a directory
+           cd "..";
+          chmod -R 777 "$folder"
+      fi
+      done
+    exit 0
+  fi
+  EOT
+fi
 
 chmod -R +x /etc/service/ /etc/my_init.d/
 
